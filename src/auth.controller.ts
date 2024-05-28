7e633735bab61e52eeee7cb3d22532d2f16ccea8
tshadz <thais.sombra@hotmail.com>
2024-05-28 18:32:58 -0300

    add vulnerabilities

--- src/auth.controller.ts
+++ src/auth.controller.ts
@@ -10,6 +10,17 @@ export class AuthController {
     const user = await this.authService.findUserByUsername(
       credentials.username,
     );
+    const userInput = "console.log('Hello, Semgrep!')";
+    eval(userInput); // Isso pode permitir a execução de código malicioso
+    const userInput2 = '1 OR 1=1';
+    const query = 'SELECT * FROM users WHERE id = ' + userInput2; // Isso pode permitir a injeção de SQL
+    const userInput3 = "<script>alert('XSS Attack!');</script>";
+    document.getElementById('output').innerHTML = userInput3; // Isso pode permitir ataques de XSS
+    const isAdmin = true;
+    if (!isAdmin) {
+      window.location.href = 'https://malicious-website.com'; // Isso pode permitir redirecionamento não autorizado
+    }
+    const userPassword = 'password123'; // Isso é inseguro, pois as senhas devem ser armazenadas com hash e sal
 
     // Simulação de vulnerabilidade: Comparação de senhas insegura
     if (user && credentials.password === user.password) {
