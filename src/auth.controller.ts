c096c55ca7ce4b71d058514113e91049c7ee17a4
tshadz <thais.sombra@hotmail.com>
2024-05-28 18:11:26 -0300

    test semgrep

--- src/auth.controller.ts
+++ src/auth.controller.ts
@@ -3,17 +3,19 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('login')
-    async login(@Body() credentials: { username: string; password: string }) {
-        const user = await this.authService.findUserByUsername(credentials.username);
+  @Post('login')
+  async login(@Body() credentials: { username: string; password: string }) {
+    const user = await this.authService.findUserByUsername(
+      credentials.username,
+    );
 
-        // Simulação de vulnerabilidade: Comparação de senhas insegura
-        if (user && credentials.password === user.password) {
-            return { accessToken: 'your-access-token' };
-        } else {
-            return { error: 'Invalid username or password' };
-        }
+    // Simulação de vulnerabilidade: Comparação de senhas insegura
+    if (user && credentials.password === user.password) {
+      return { accessToken: 'your-access-token' };
+    } else {
+      return { error: 'Invalid username or password' };
     }
+  }
 }
