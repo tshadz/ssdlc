ffec0e7ac7ffef0e2ebe1070dd1d64eab386cb97
tshadz <thais.sombra@hotmail.com>
2024-05-28 17:59:18 -0300

    add semgrep vulnerability

---
+++ src/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    async login(@Body() credentials: { username: string; password: string }) {
+        const user = await this.authService.findUserByUsername(credentials.username);
+
+        // Simulação de vulnerabilidade: Comparação de senhas insegura
+        if (user && credentials.password === user.password) {
+            return { accessToken: 'your-access-token' };
+        } else {
+            return { error: 'Invalid username or password' };
+        }
+    }
+}
